05669ba9cc32d4d4fe78a211289dec782b94e724
7-wolf <790951046@qq.com>
2018-11-19 15:39:55 +0800

    isXXX

--- js/utils/Util.js
+++ js/utils/Util.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { AsyncStorage } from 'react-native';
-export class _Util{
+
+export class Util{
     static isElement(any){return React.isValidElement(any);}
-    static isString(any){return typeof(any) == "string"}
-    static isNumber(any){return typeof(any) == "number"}
-    static isObject(any){return typeof(any) == "object"}
-    static isBoolean(any){return typeof(any) == "boolean"}
-    static isUndefined(any){return typeof(any) == "undefined"}
+    static isString(any){return typeof(any) === "string"}
+    static isNumber(any){return typeof(any) === "number"}
+    static isObject(any){return typeof(any) === "object"}
+    static isBoolean(any){return typeof(any) === "boolean"}
+    static isUndefined(any){return typeof(any) === "undefined"}
     static isArray(arr){return Array.isArray(arr);}
     static isFloat(str){ return /^[0-9]+.?[0-9]*$/.test(str);}
     static replaceInt(str){return str.replace(/[^\d]/g, "");}
     static replaceFloat(str){return str.replace(/[^\a-\z\A-\Z0-9]/g, "");}
     static replaceDAZ(str){return str.replace(/[^\a-\z\A-\Z0-9]/g, "");}
-    static toDate(t, symbol, cut) {
+    static stringify(text){return this.isObject(text) ? JSON.stringify(text) : text;}
+    
+    static toDate(ti, symbol, cut_link) {
         symbol = symbol || "-";
-        let date = new Date(t*1000);
-        t = n=>(n>9)?n:("0"+n);
-        symbol = date.getFullYear() + symbol + t(date.getMonth()) + symbol + t(date.getDate());
-        if(this.isBoolean(cut) && cut){
+        let date = new Date(ti*1000);
+        ti = n=>(n>9)?n:("0"+n);
+        symbol = date.getFullYear() + symbol + ti(date.getMonth()) + symbol + ti(date.getDate());
+        if(this.isBoolean(cut_link) && cut_link){
             return symbol;
         }
-        cut = cut || " ";
-        return symbol + cut + date.toTimeString().substr(0, 8)
+        cut_link = cut_link || " ";
+        return symbol + cut_link + date.toTimeString().substr(0, 8)
     }
 
     static toFile(uri){
@@ -38,17 +40,15 @@ export class _Util{
         return null;
     }
 
-    static save(key, info) {
-        AsyncStorage.setItem(key, JSON.stringify(info), (error) => {
-            if (error) {
-                Dialog.toast(error.message)
-            }
-        });
-    }
-
-    static load(key, cb) {
-        AsyncStorage.getItem(key, (error, result) => {
-            cb(error ? null : JSON.parse(result));
-        })
+    static checkInterger(str){
+        let str2 = str;
+        if(str2.length < 0){
+            return "输入不能为空";
+        }
+        str2 = parseInt(str2);
+        if(str2 < 1 || str2.toString() != str){
+            return "请输入整数";
+        }
+        return ""
     }
 }
