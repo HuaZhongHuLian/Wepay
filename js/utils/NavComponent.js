fb2a6fe56051f604dea44c9c9d3bcef54cdfcc86
7-wolf <790951046@qq.com>
2018-11-21 17:08:36 +0800

    导航组件

--- js/utils/NavComponent.js
+++ js/utils/NavComponent.js
@@ -1,24 +1,33 @@
-import React from "react";
-import { Navigator } from "./Navigator";
-import { Net } from "./Component";
+import React from "react"
+import { Navigator } from "./Navigator"
+import { Net } from "./Net"
+import { Jx } from "./Jx";
 
 export class NavComponent extends React.Component{
     constructor(props){
         super(props);
-        Navigator.setNavigator(this.props.navigation);
+        Navigator.push(this.props.navigation);
         this.onLeft = this.onLeft.bind(this);
+        // console.log(JSON.stringify(this.props.navigation));
     }
 
     onLeft(){
         Navigator.navigateBack(this.props.navigation);
     }
 
+    getNav(){
+        return this.props.navigation;
+    }
+
     getParams(){
         return this.props.navigation.state.params;
     }
 
-    componentDidMount(){
+    getRouteName(){
+        return this.props.navigation.state.routeName;
+    }
 
+    componentDidMount(){
     }
 
     renavigate(page){
@@ -29,12 +38,23 @@ export class NavComponent extends React.Component{
         Navigator.navigate(this.props.navigation, page, obj);
     }
 
-
-    fet(url, obj, cb1){
-        Net.fet(url, obj, cb1, this.props.navigation);
+    fet(url, obj){
+        return Net.fet(this.props.navigation.state.routeName, url, obj);
     }
 
-    fetid(url, obj, cb1){
-        Net.fetid(url, obj, cb1, this.props.navigation);
+    fetid(url, obj){
+        return Net.fetid(this.props.navigation.state.routeName, url, obj);
+    }
+
+    is24(r, notNavigate){
+        if(r.is1){
+            return false;
+        }
+        if(r.code === 2 || r.code === 4){
+            if(Jx.isUndefined(notNavigate) || (!notNavigate)){
+                Navigator.renavigate(this.props.navigation, "LoginPage");
+            }
+        }
+        return true;
     }
 }
